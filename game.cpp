2c178d2f0c5bd0ec92a9038b17f8ec07f85e652c
Shan <aragornolivier@gmail.com>
2021-11-21 14:51:50 +0100

    Create game.cpp

---
+++ game.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <memory>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+class Game{
+    /*********
+    Singleton code from class INFO-F-202
+    **********/
+    static unique_ptr<Game> singleton;
+    ofstream fileSprite;
+    vector<ofstream> candySprite;
+
+    Game(): fileSprite{"Sprites"} {};
+
+    public:
+
+    void drawBoard(const string message)
+    {
+        cout << message;
+    }
+
+    static Game &create(){
+        if (!singleton)
+            singleton=unique_ptr<Game>(new Game());
+            return *singleton;
+    }
+
+};
+
+unique_ptr<Game> Game::singleton;
+
+int nain()
+{
+    Game::create().drawBoard("Bonjour");
+    return 0;
+}

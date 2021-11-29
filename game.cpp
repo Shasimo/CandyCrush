f823b807b99a8b2f6b65a8c4682359900f5c9f41
Shan <aragornolivier@gmail.com>
2021-11-29 12:52:42 +0100

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -2,38 +2,72 @@
 #include <memory>
 #include <vector>
 #include <fstream>
+#include <dirent.h>
+#include </home/guest/Documents/C/CandyCrush/CandyCrush/draw.cpp>
 
 using namespace std;
 
-class Game{
-    /*********
-    Singleton code from class INFO-F-202
-    **********/
-    static unique_ptr<Game> singleton;
-    ofstream fileSprite;
-    vector<ofstream> candySprite;
+/*--------------------------------------------------
+ 
+Interface: Create Interface
+ 
+--------------------------------------------------*/
 
-    Game(): fileSprite{"Sprites"} {};
+class Interface
+ {
+   static unique_ptr<Interface> singleton;
+   Interface() {};
 
-    public:
+   public:
 
-    void drawBoard(const string message)
-    {
-        cout << message;
-    }
+   static Interface &create(){
+       if (!singleton)
+       singleton=unique_ptr<Interface>(new Interface());
+       return *singleton;}
 
-    static Game &create(){
-        if (!singleton)
-            singleton=unique_ptr<Game>(new Game());
-            return *singleton;
-    }
+   void getWindow(int argc, char *argv[]);
+   vector <char*> receivePNGFromSpriteDir();
+    
+ };
 
-};
 
-unique_ptr<Game> Game::singleton;
 
-int main()
+unique_ptr<Interface> Interface::singleton;
+
+
+void Interface::getWindow(int argc, char *argv[]) {     
+     auto candySpritesVector=receivePNGFromSpriteDir();
+
+     srand(time(0));
+     MainWindow window(candySpritesVector);
+     window.show(argc, argv);
+     Fl::run();
+}
+
+
+vector <char*> Interface::receivePNGFromSpriteDir(){
+  vector <char*> v;
+
+  struct dirent *entry=nullptr;
+  DIR *dp = nullptr;
+  dp = opendir("Sprites");
+  while((entry = readdir(dp))){
+
+  string candySprite=entry->d_name;
+  char* candySprite_2=entry->d_name;
+
+  if(candySprite.find(".png") != string::npos){
+  v.push_back(candySprite_2);
+  }
+  }
+  return v;
+}
+
+/*****
+*****/
+
+int main(int argc, char *argv[])
 {
-    Game::create().drawBoard("Bonjour");
+    Interface::create().getWindow(argc, argv);
     return 0;
 }
